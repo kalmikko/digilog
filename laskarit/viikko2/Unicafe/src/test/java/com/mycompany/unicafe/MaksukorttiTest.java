7cd11a197a1dc4e52bc082214fdf809db0a43ea9
Kalle Mikkola <kalle.mikkola@helsinki.fi>
2019-03-18 12:52:56 +0200

    vko2:unicafetestit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,27 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void luodunKortinSaldoOikea() {
+        assertEquals("saldo: 0.10", kortti.toString());      
+    }
+    
+    @Test
+    public void kortinSaldoOikeinKunLataaRahaa() {
+        kortti.lataaRahaa(500);
+        assertEquals("saldo: 5.10", kortti.toString());  
+    }
+    
+    @Test
+    public void kortinSaldoOikeaKunOttaaRahaa() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.05", kortti.toString());  
+    }
+    
+    @Test
+    public void kortinSaldoEiMuutuJosseiRahaa() {
+        kortti.otaRahaa(100);
+        assertEquals("saldo: 0.10", kortti.toString());  
+    }
 }
